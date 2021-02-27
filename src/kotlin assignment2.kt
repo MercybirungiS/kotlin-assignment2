d17bd19722ddb217b1e548b7b635d67a07ddd63e
MercybirungiS <mercyssbirungi@gmail.com>
2021-02-28 00:32:38 +0300

    kotlin assignment2

---
+++ src/kotlin assignment2.kt
@@ -0,0 +1,25 @@
+fun main() {
+    name()
+    modulus()
+    var add=addition(1,3,5,7)
+    println(add )
+    interestingFact()
+}
+fun name () {
+    var name=("hello mercy")
+    println(name )
+}
+fun modulus (){
+    var a=7
+    var b=3
+    var modulus=a%b
+    println(modulus )
+}
+fun addition(num1:Int,num2:Int,num3:Int,num4:Int ):Int {
+    var add=num1 + num2 + num3 + num4
+    return add
+}
+fun interestingFact(){
+    var interestingFact=("i never give up")
+    println(interestingFact).
+}
